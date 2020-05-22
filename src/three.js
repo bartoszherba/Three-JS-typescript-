6e7140342b5280350f63d6f5e5380fe54dc4b037
Bartosz Herba <bherba@divante.pl>
2020-05-22 19:52:20 +0200

    Add eslinter

--- src/three.js
+++ src/three.js
@@ -7,24 +7,18 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 camera.position.z = 5;
 
 function animate() {
-  requestAnimationFrame( animate );
-  
+  requestAnimationFrame(animate);
+
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
-	renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
-
-
-function logKey(e) {
-  cube.rotation.x += 0.1;
-}
-
 
 animate();
